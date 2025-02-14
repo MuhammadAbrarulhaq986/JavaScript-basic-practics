9c4a5b69b4d1b3480ac3c3f72e0b1ec360e13ccb
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2025-02-14 18:01:58 +0500

    Data Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict"; //* TREAT ALL JS CODE AS NEWER VERSION of JS
+
+// alert(3 + 3); //* WE ARE USING NODE JS NOT BROWSER
+
+console.log(
+    3
+    +
+    3) //* Code readability should be highly prioritized
+
+
+console.log("Hello")
+
+
+
+let name = "Aman"
+let age = 22
+let isLoggedIn = false
+let state = null
+
+//* number => 2 to power 53
+//* bigInt : IT'S USE IN STOCKS MARKIT, TRADING, etc
+//* string => "" or ''
+//* boolean => true/false
+//* null => standalone value
+//* undefined =>
+//* symbol => unique
+
+
+//* object
+//console.log(typeof "Abrar") //* string
+//console.log(typeof age) //* number
+console.log(typeof null) //* object
+console.log(typeof undefined) //* undefined
+
