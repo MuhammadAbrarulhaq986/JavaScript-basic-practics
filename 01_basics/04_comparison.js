4ec576630d788e9fffc653ec74e1a5ad3f20e7ac
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2025-02-19 22:33:42 +0500

    Comparison of datatypes in javascript

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,30 @@
+//!!!!!!! BASIC CONVERSION  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//console.log(2 > 1);//* TRUE
+//console.log(2 >= 1);//* TRUE
+//console.log(2 < 1);//* FALSE
+//console.log(2 == 1);//* FALSE
+//console.log(2 != 1); //* TRUE
+
+//!!!!!! COMPARING 2 DIFFERENT DATA TYPES STRING TO NUMBER !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//* TRY TO AVOID THESE TYPES OF console.log
+//console.log("2" > 1); //* TRUE
+//console.log("02" > 1); //* TRUE
+
+//* TRY TO AVOID THESE TYPES OF console.log
+//console.log(null > 0); //* FALSE
+//console.log(null == 0); //* FALSE
+//console.log(null >= 0); //* TRUE
+//console.log(null < 0); //* FALSE
+//console.log(null <= 0); //* TRUE
+
+//* WHEN WE TEST USING undefined THE ANSWER WILL COME IN FALSE
+//* TRY TO AVOID THESE TYPES OF console.log
+//console.log(undefined == 0); 
+//console.log(undefined >= 0);
+//console.log(undefined > 0);
+//console.log(undefined < 0);
+//console.log(undefined <= 0);
+
+
+//* === ( TRIPLE EQUALS TO) STRICKLE CHECKS THE CONDITIONS AND DATATYPES
+console.log("2" === 2);
