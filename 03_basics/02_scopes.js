e35fc6605e23b78c7a85601c1a800c4ef8cf1749
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2025-03-31 23:47:05 +0500

    Scope level and mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -11,7 +11,7 @@ if (true) {
     let a = 10;
     const b = 20;
 
-    console.log("INNER: ", a);
+    //console.log("INNER: ", a);
 
 
     //c = 30;
@@ -21,6 +21,48 @@ if (true) {
 
 }
 
-console.log(a);
+//console.log(a);
 //console.log(b);
 //console.log(c);
+
+function one() {
+    const username = "Abrar"
+
+    function two() {
+        const website = "youtube"
+        //console.log(username);
+    }
+    //console.log(website);//* the execution will display undefined because this is out side of the scope
+    two()//* if you comment this it wont be able to excute the function
+}
+//one()
+
+if (true) {
+    const username = "Abrar"
+    if (username === "Abrar") {
+        const website = " youtube"
+        //console.log(username + website);
+    }
+    //console.log(website);//* if you log it out side of a scope it will give not-defined
+}
+//console.log(username); //* if you log it out side of a scope it will give not-defined
+
+//************ interesting **********************************/
+
+//* THIS IS A FUNCTION
+//* YOU CAN ACCESS THIS before initialization 
+//addOne(5)
+console.log(addOne(5));
+
+function addOne(num) {
+    return num + 1
+}
+//addOne(5)
+
+//* THIS ALSO A FUNCTION BUT SOME TIMES WE CALL IT EXPRESSION.
+
+//addTwo(5)//* ReferenceError: Cannot access 'addTwo' before initialization
+const addTwo = function (num) {
+    return num + 2
+}
+addTwo(5)
